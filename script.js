cfe6b3c202647843ae57590d7f173ce228dde207
miizannnn <mzulhamizanhailmi@gmail.com>
2026-07-07 16:01:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
-// Smooth scrolling
-document.querySelectorAll('a').forEach(link=>{
+const galleries = {
 
-link.addEventListener('click',function(e){
+porsche:[
 
-e.preventDefault();
+"https://images.unsplash.com/photo-1503376780353-7e6692767b70",
 
-document.querySelector(this.getAttribute('href'))
-.scrollIntoView({
-behavior:"smooth"
+"https://images.unsplash.com/photo-1544829099-b9a0c07fad1a"
+
+],
+
+
+bmw:[
+
+"https://images.unsplash.com/photo-1503736334956-4c8f8e92946d",
+
+"https://images.unsplash.com/photo-1492144534655-ae79c964c9d7"
+
+],
+
+
+track:[
+
+"https://images.unsplash.com/photo-1492144534655-ae79c964c9d7"
+
+]
+
+};
+
+
+
+function openGallery(name){
+
+let gallery = document.getElementById("gallery-images");
+
+gallery.innerHTML="";
+
+
+galleries[name].forEach(image=>{
+
+gallery.innerHTML += `<img src="${image}">`;
+
 });
 
-});
 
-});
+document.getElementById("lightbox").style.display="block";
+
+}
+
+
+
+function closeGallery(){
+
+document.getElementById("lightbox").style.display="none";
+
+}
